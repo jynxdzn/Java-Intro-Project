423b76e8ad303fc81fe1985f8fbedd8c50cd9f1d
Kemel McKenzie <kemel@pixelacademy.org>
2015-06-09 11:45:29 -0700

    I'll explain everything in my next push.
    kinda made a mess lmfao.

---
+++ Intro-Project/src/project/MainProject.java
@@ -0,0 +1,17 @@
+package project;
+
+public class MainProject {
+
+	public static void main(String[] args) {
+		String Name = "Jacob";
+		int Age = 10;
+		// TODO Auto-generated method stub
+		if (Age  == 10) {
+			System.out.println("You're In The 5th Grade.");
+		} else { 
+			System.out.println("You're Not In The 5th Grade.");
+		}
+
+	}
+
+}
---
+++ Intro-Project/src/project/main.java
@@ -0,0 +1,6 @@
+package project;
+
+public class main {
+	String Name = "Kemel";
+	int Age = 10; 
+}
